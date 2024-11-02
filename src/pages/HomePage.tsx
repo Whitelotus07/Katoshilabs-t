f00791b857bceeb65e9d35f639ceef8c72931754
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-02 20:32:30 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
+// src/pages/HomePage.tsx
 import React from 'react';
 import BinaryBackground from '../components/BinaryBackground';
-import Header from '../components/Header';
-import HeroSection from '../components/HeroSection';
-import ProductSection from '../components/ProductSection';
-import ServicesSection from '../components/ServicesSection';
-import AboutSection from '../components/AboutSection';
-import ContactSection from '../components/ContactSection';
+import { Header } from '../components/Header';
+import { HeroSection } from '../components/HeroSection';
+import { ProductSection } from '../components/ProductSection';
+import { ServicesSection } from '../components/ServicesSection';
+import { AboutSection } from '../components/AboutSection';
+import { ContactSection } from '../components/ContactSection';
 
 const HomePage: React.FC = () => {
   return (
