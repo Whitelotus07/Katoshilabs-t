b6de8277d49edb96db5dbe1f199cd36899af3e80
Whitelotus07 <josephkazumi7@gmail.com>
2024-11-03 02:10:19 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+// src/pages/HomePage.tsx
 import React from 'react';
 import BinaryBackground from '../components/BinaryBackground';
 import { Header } from '../components/Header';
@@ -10,19 +11,19 @@ import { ContactSection } from '../components/ContactSection';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <BinaryBackground />
-      <div className="relative z-10 flex-grow">
+      <div className="relative z-10">
         <Header />
-        <main className="container mx-auto px-6">
+        <main>
           <HeroSection />
           <ProductSection />
           <ServicesSection />
           <AboutSection />
           <ContactSection />
         </main>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
